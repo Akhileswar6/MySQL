77c210c3417c39827af41e0cb5973be53a42a499
Akhileswar <akhilkamale@gmail.com>
2025-11-22 23:34:29 +0530

    initial commit

---
+++ 06_Where_clause.sql
@@ -0,0 +1,10 @@
+USE college;
+
+
+-- WHERE clause examples to filter data from the students table
+SELECT * FROM students WHERE age >= 21;
+
+SELECT * FROM students WHERE city = "New York";
+
+SELECT * FROM students WHERE age BETWEEN 20 AND 23;
+
