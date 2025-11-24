e3c3c5e3964f7222f8c635c5849ec43c1277c4bc
Akhileswar <akhilkamale@gmail.com>
2025-11-24 10:10:24 +0530

    initial commit

---
+++ 07_Order_by_clause.sql
@@ -0,0 +1,8 @@
+USE students;
+
+-- ORDER BY clause examples to sort data from the students table
+SELECT * FROM students ORDER BY name;
+
+SELECT * FROM students ORDER BY age DESC;
+
+SELECT * FROM students ORDER BY age ASC;
