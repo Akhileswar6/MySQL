167458223b0696de73748cd92ceb9ec09487d106
Akhileswar <akhilkamale@gmail.com>
2025-11-29 19:54:39 +0530

    updated file

--- 05_Select_query.sql
+++ 05_Select_query.sql
@@ -31,3 +31,42 @@ WHERE age BETWEEN 20 AND 30;
 
 SELECT name AS student_name, city AS location
 FROM students;
+
+
+
+-- Scenario Based:
+SELECT name, salary FROM employees 
+WHERE salary > 50000;
+
+
+SELECT * FROM products 
+WHERE category = "Electronics";
+
+SELECT *  FROM students
+ORDER BY score DESC
+LIMIT 5;
+
+SELECT COUNT(*) AS total_students
+FROM orders;
+
+SELECT customer_name, email
+FROM customers
+WHERE city IN ("Chennai","Bangalore");
+
+SELECT DISTINCT category
+FROM products;
+
+
+SELECT dept, AVG(salary) AS avg_salary
+FROM employees
+GROUP BY dept;
+
+
+SELECT * 
+FROM users
+WHERE last_login >= DATE_SUB(CURDATE(), INTERVAL 7 DAY);
+
+
+SELECT product_name, quantity
+FROM products
+WHERE quantity < 10;
