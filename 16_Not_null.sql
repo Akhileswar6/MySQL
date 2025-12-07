a1bfa72839e05ae50131fd91318e970ae81db42c
Akhileswar Kamale <akhilkamale@gmail.com>
2025-12-07 21:13:29 +0530

    initial commit

---
+++ 16_Not_null.sql
@@ -0,0 +1,11 @@
+USE college
+
+-- NOT NULL CONSTRAINT ensures the column cannot be empty
+CREATE TABLE student3(
+    name VARCHAR(50) NOT NULL
+);
+
+
+INSERT INTO student3 VALUES
+("akhil"),
+("shankar");
