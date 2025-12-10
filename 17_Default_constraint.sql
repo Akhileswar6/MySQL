99db8f975bba8e4e000ce043d8fcfe317c0f6260
Akhileswar Kamale <akhilkamale@gmail.com>
2025-12-11 00:02:43 +0530

    initial commit

---
+++ 17_Default_constraint.sql
@@ -0,0 +1,8 @@
+USE college;
+
+-- DEFAULT Constraint assigns a default value automatically if the user does not provide one.
+CREATE TABLE student4 (
+    city VARCHAR(50) DEFAULT 'Chennai'
+);
+
+
