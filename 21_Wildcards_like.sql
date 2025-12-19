ea35ead93d8005b04b37fc4aee0915b0dc863de6
Akhileswar Kamale <akhilkamale@gmail.com>
2025-12-19 07:57:32 +0530

    added new sql data

---
+++ 21_Wildcards_like.sql
@@ -0,0 +1,13 @@
+USE college;
+
+-- Wildcards are characters used in pattern matching inside a LIKE condition.
+
+--  % → Matches any number of characters
+SELECT * FROM students WHERE name LIKE 'A%';      -- starts with A
+SELECT * FROM students WHERE name LIKE '%h';      -- ends with h
+SELECT * FROM students WHERE name LIKE '%na%';    -- contains na in "name"
+
+
+-- _ → Matches exactly one character
+SELECT * FROM students WHERE name LIKE '_a%';        -- second letter is 'a'
+SELECT * FROM students WHERE name LIKE '_____';      -- names with exactly 5 letter
